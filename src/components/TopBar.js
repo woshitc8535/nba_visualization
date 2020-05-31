5fe49ddb1406744a208e1d54041893fe500413e9
liu Wenxuan <woshitc8535@163.com>
2020-05-30 23:56:27 -0400

    nba

---
+++ src/components/TopBar.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import logo from '../assets/images/nba-logoman-word-white.png'
+
+class TopBar extends Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+
+
+                </header>
+            </div>
+        );
+    }
+}
+
+export default TopBar;
